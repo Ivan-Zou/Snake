48bdfdd6e471d76226be5f665420945b3accf100
Ivan Zou <ivanzou2@gmail.com>
2022-06-01 12:08:52 -0400

    added window location and field width and height to Constants

--- src/snake/misc/Constants.java
+++ src/snake/misc/Constants.java
@@ -1,8 +1,10 @@
 package snake.misc;
 
 public class Constants {
-
-
+    public static final int WINDOW_LOCATION_X = 445;
+    public static final int WINDOW_LOCATION_Y = 75;
+    public static final int FIELD_WIDTH = 400;
+    public static final int FIELD_HEIGHT = 400;
 
     public static final String HIGH_SCORE_FILE = "files/highscore.txt";
     public static final String SCORES_FILE = "files/scores.txt";
