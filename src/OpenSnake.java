af6ae88f6dd5edfd1f929ffe6a8b85cbb510b02a
Ivan Zou <ivanzou2@gmail.com>
2022-06-01 11:16:16 -0400

    finished OpenSnake

--- src/OpenSnake.java
+++ src/OpenSnake.java
@@ -1,2 +1,10 @@
+import snake.RunSnake;
+
+import javax.swing.SwingUtilities;
+
 public class OpenSnake {
+    public static void main(String[] args) {
+        Runnable snake = new RunSnake();
+        SwingUtilities.invokeLater(snake);
+    }
 }
