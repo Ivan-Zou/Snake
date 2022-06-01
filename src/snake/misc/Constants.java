245cd833f1220594fc426532887baf31195572a7
Ivan Zou <ivanzou2@gmail.com>
2022-06-01 11:09:54 -0400

    added scores and highscores file addresses to Constants

--- src/snake/misc/Constants.java
+++ src/snake/misc/Constants.java
@@ -1,4 +1,9 @@
 package snake.misc;
 
 public class Constants {
+
+
+
+    public static final String HIGH_SCORE_FILE = "files/highscore.txt";
+    public static final String SCORES_FILE = "files/scores.txt";
 }
