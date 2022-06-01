3bd8d02f348cfb18050e39f5d19eba7f6870f032
Ivan Zou <ivanzou2@gmail.com>
2022-06-01 12:09:50 -0400

    set up the GUI

--- src/snake/RunSnake.java
+++ src/snake/RunSnake.java
@@ -1,4 +1,45 @@
 package snake;
 
-public class RunSnake {
+import snake.gamedata.Scores;
+import snake.mechanics.GameMechanics;
+import snake.misc.Constants;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+import java.awt.BorderLayout;
+
+public class RunSnake implements Runnable {
+    @Override
+    public void run() {
+        final JFrame frame = new JFrame("Snake");
+
+        final JPanel scorePanel = new JPanel();
+        frame.add(scorePanel, BorderLayout.NORTH);
+
+        final JLabel scoreLabel = new JLabel("Score: " + 0);
+        scoreLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        scorePanel.add(scoreLabel);
+
+        final Scores scores = new Scores();
+        final JLabel highScoreLabel = new JLabel("High Score: " + scores.getHighScore());
+        highScoreLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        scorePanel.add(highScoreLabel);
+
+        final GameMechanics snake = new GameMechanics(scoreLabel);
+        frame.add(snake, BorderLayout.CENTER);
+
+        final JPanel statusPanel = new JPanel();
+        frame.add(statusPanel, BorderLayout.SOUTH);
+
+        final JLabel statusLabel = new JLabel("Running...");
+        statusPanel.add(statusLabel);
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocation(Constants.WINDOW_LOCATION_X, Constants.WINDOW_LOCATION_Y);
+    }
 }
--- src/snake/mechanics/GameMechanics.java
+++ src/snake/mechanics/GameMechanics.java
@@ -1,4 +1,30 @@
 package snake.mechanics;
 
-public class GameMechanics {
+import snake.misc.Constants;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+
+public class GameMechanics extends JPanel {
+
+    public GameMechanics(JLabel score) {
+
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(Constants.FIELD_WIDTH, Constants.FIELD_HEIGHT);
+    }
 }
