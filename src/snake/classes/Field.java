5b7523719e74e1cd3e409db73d1f2626a64e8727
Ivan Zou <ivanzou2@gmail.com>
2022-06-02 10:08:05 -0400

    progress on Field class - added variables, complete and incomplete methods, and a constructor.

--- src/snake/classes/Field.java
+++ src/snake/classes/Field.java
@@ -1,4 +1,41 @@
 package snake.classes;
 
+import snake.misc.Constants;
+
+import java.awt.Graphics;
+
 public class Field {
+    private final Snake snake;
+    private int[][] field;
+    private int points;
+
+    public Field(Snake snake) {
+        this.snake = snake;
+        reset();
+    }
+
+    public void reset() {
+        field = new int[Constants.FIELD_SIZE][Constants.FIELD_SIZE];
+        points = 0;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void update() {
+
+    }
+
+    public boolean gameOver() {
+        return false;
+    }
+
+    public int getCell(int x, int y) {
+        return field[y][x];
+    }
+
+    public void draw(Graphics g) {
+
+    }
 }
