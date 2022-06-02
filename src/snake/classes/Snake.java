d13aa3e8baee12878bb932e80d7c7ef98c986c99
Ivan Zou <ivanzou2@gmail.com>
2022-06-02 08:33:35 -0400

    made Point inner class in Snake class

--- src/snake/classes/Snake.java
+++ src/snake/classes/Snake.java
@@ -1,4 +1,24 @@
 package snake.classes;
 
+import snake.misc.Direction;
+
+import java.util.LinkedList;
+
 public class Snake {
+    private LinkedList<Point> snake;
+
+    public Snake() {
+        snake = new LinkedList<>();
+    }
+}
+
+class Point {
+    int x, y;
+    Direction direction;
+
+    Point(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
 }
