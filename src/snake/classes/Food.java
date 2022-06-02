321f19e85d5deacafd734d605b7b5a4208ba4d7f
Ivan Zou <ivanzou2@gmail.com>
2022-06-02 11:39:14 -0400

    added Food class

---
+++ src/snake/classes/Food.java
@@ -0,0 +1,41 @@
+package snake.classes;
+
+import snake.misc.Constants;
+import snake.misc.Direction;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Food {
+    private final Color color;
+    private final Point position;
+
+    public Food() {
+        color = Color.RED;
+        position = new Point(-1, -1, Direction.NONE);
+    }
+
+    public void generate() {
+        position.x = getRandomInt(0, Constants.FIELD_SIZE - 1);
+        position.y = getRandomInt(0, Constants.FIELD_SIZE - 1);
+    }
+
+    private static int getRandomInt(int min, int max) {
+        return BigDecimal.valueOf((int) ((Math.random() * (max - min)) + min))
+                .setScale(0, RoundingMode.HALF_UP).intValue();
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(position.x * Constants.CELL_SIZE,
+                position.y * Constants.CELL_SIZE,
+                Constants.CELL_SIZE,
+                Constants.CELL_SIZE);
+    }
+}
