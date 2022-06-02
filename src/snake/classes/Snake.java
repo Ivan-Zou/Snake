b81905df1d7d421c06691c7a4bf2b3ab06818498
Ivan Zou <ivanzou2@gmail.com>
2022-06-02 10:12:06 -0400

    progress on Snake class - added variables, a constructor, and complete and incomplete methods.

--- src/snake/classes/Snake.java
+++ src/snake/classes/Snake.java
@@ -1,14 +1,58 @@
 package snake.classes;
 
+import snake.misc.Constants;
 import snake.misc.Direction;
 
+import java.awt.Graphics;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Snake {
-    private LinkedList<Point> snake;
+    private final LinkedList<Point> snake;
+    private Direction direction;
 
     public Snake() {
         snake = new LinkedList<>();
+        reset();
+    }
+
+    public void reset() {
+        direction = Direction.NONE;
+        snake.clear();
+        snake.add(new Point(Constants.SNAKE_STARTING_HEAD_X,
+                Constants.SNAKE_STARTING_HEAD_Y, Direction.RIGHT));
+        snake.add(new Point(Constants.SNAKE_STARTING_BODY_X,
+                Constants.SNAKE_STARTING_BODY_Y, Direction.RIGHT));
+        snake.add(new Point(Constants.SNAKE_STARTING_TAIL_X,
+                Constants.SNAKE_STARTING_TAIL_Y, Direction.RIGHT));
+    }
+
+    public void move() {
+
+    }
+
+    public void up() {
+        direction = Direction.UP;
+    }
+
+    public void down() {
+        direction = Direction.DOWN;
+    }
+
+    public void left() {
+        direction = Direction.LEFT;
+    }
+
+    public void right() {
+        direction = Direction.RIGHT;
+    }
+
+    public Iterator<Point> iterator() {
+        return snake.iterator();
+    }
+
+    public void draw(Graphics g) {
+
     }
 }
 
