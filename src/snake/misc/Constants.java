fb053a36b5b3088b2a62c14b39fe238f8bbb7bcc
Ivan Zou <ivanzou2@gmail.com>
2022-06-02 10:05:16 -0400

    added field size, snake starting positions, and refresh rate to Constants

--- src/snake/misc/Constants.java
+++ src/snake/misc/Constants.java
@@ -6,6 +6,17 @@ public class Constants {
     public static final int FIELD_WIDTH = 400;
     public static final int FIELD_HEIGHT = 400;
 
+    public static final int FIELD_SIZE = 20;
+
+    public static final int SNAKE_STARTING_HEAD_X = 4;
+    public static final int SNAKE_STARTING_HEAD_Y = 10;
+    public static final int SNAKE_STARTING_BODY_X = 3;
+    public static final int SNAKE_STARTING_BODY_Y = 10;
+    public static final int SNAKE_STARTING_TAIL_X = 2;
+    public static final int SNAKE_STARTING_TAIL_Y = 10;
+
+    public static final int REFRESH_RATE = 1;
+
     public static final String HIGH_SCORE_FILE = "files/highscore.txt";
     public static final String SCORES_FILE = "files/scores.txt";
 }
