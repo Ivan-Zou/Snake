d25d4ed5c447b88ce8ac737954269b26beff9286
Ivan Zou <ivanzou2@gmail.com>
2022-06-03 11:08:42 -0400

    added Label color, cell size, and delay Constants

--- src/snake/misc/Constants.java
+++ src/snake/misc/Constants.java
@@ -1,10 +1,14 @@
 package snake.misc;
 
+import java.awt.Color;
+
 public class Constants {
     public static final int WINDOW_LOCATION_X = 445;
     public static final int WINDOW_LOCATION_Y = 75;
     public static final int FIELD_WIDTH = 400;
     public static final int FIELD_HEIGHT = 400;
+
+    public static final Color LABEL_COLOR = Color.GRAY;
 
     public static final int FIELD_SIZE = 20;
 
@@ -15,7 +19,10 @@ public class Constants {
     public static final int SNAKE_STARTING_TAIL_X = 2;
     public static final int SNAKE_STARTING_TAIL_Y = 10;
 
-    public static final int REFRESH_RATE = 1;
+    public static final int CELL_SIZE = 20;
+
+    public static final int REFRESH_RATE = 120;
+    public static final int DELAY = 100;
 
     public static final String HIGH_SCORE_FILE = "files/highscore.txt";
     public static final String SCORES_FILE = "files/scores.txt";
