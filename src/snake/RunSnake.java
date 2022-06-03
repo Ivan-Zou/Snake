882db7d44b27e7029b963f3c43842d81e2612f5c
Ivan Zou <ivanzou2@gmail.com>
2022-06-03 11:13:36 -0400

    set background panel color and label color

--- src/snake/RunSnake.java
+++ src/snake/RunSnake.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.BorderFactory;
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 public class RunSnake implements Runnable {
     @Override
@@ -16,21 +17,26 @@ public class RunSnake implements Runnable {
         final JFrame frame = new JFrame("Snake");
 
         final JPanel scorePanel = new JPanel();
+        scorePanel.setBackground(Color.BLACK);
         frame.add(scorePanel, BorderLayout.NORTH);
 
         final JLabel scoreLabel = new JLabel("Score: " + 0);
+        scoreLabel.setForeground(Constants.LABEL_COLOR);
         scoreLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
         scorePanel.add(scoreLabel);
 
         final Scores scores = new Scores();
         final JLabel highScoreLabel = new JLabel("High Score: " + scores.getHighScore());
+        highScoreLabel.setForeground(Constants.LABEL_COLOR);
         highScoreLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
         scorePanel.add(highScoreLabel);
 
         final JPanel statusPanel = new JPanel();
+        statusPanel.setBackground(Color.BLACK);
         frame.add(statusPanel, BorderLayout.SOUTH);
 
         final JLabel statusLabel = new JLabel("Running...");
+        statusLabel.setForeground(Constants.LABEL_COLOR);
         statusPanel.add(statusLabel);
 
         final GameMechanics snake = new GameMechanics(scoreLabel, highScoreLabel, statusLabel);
