219a1cdb95cadb59d581886f3151bc35d00cf827
Ivan Zou <ivanzou2@gmail.com>
2022-12-23 12:28:39 -0500

    set location of window to the center

--- src/snake/RunSnake.java
+++ src/snake/RunSnake.java
@@ -46,6 +46,6 @@ public class RunSnake implements Runnable {
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocation(Constants.WINDOW_LOCATION_X, Constants.WINDOW_LOCATION_Y);
+        frame.setLocationRelativeTo(null);
     }
 }
